38c36c5874ea8d9cc8d662723c9f51ccb48946eb
Mai <mainguyen2913@gmail.com>
2025-12-01 21:44:49 +0700

    first commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import flashcardRoutes from "./routes/flashcard.routes.js";
 import { errorHandler } from "./middlewares/error.js";
 
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(cors({ origin: process.env.CLIENT_ORIGIN || "*", credentials: true }));
 
 app.get("/", (req, res) => res.json({ ok: true, service: "japanese-ai-backend" }));
 app.use("/api/auth", authRoutes);
+app.use("/api/flashcards", flashcardRoutes);
 
 app.use(errorHandler);
 export default app;
